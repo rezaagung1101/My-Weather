5d65af8ebb5d0b06ade70211ea4412e5c5f74223
rezaagung1101 <rezaagung3910@gmail.com>
2024-03-20 14:36:37 +0700

    add: main layout

--- app/src/main/java/com/jetpack/compose/myweather/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/myweather/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * 1. New York 2. Singapore 3. Mumbai 4. Delhi 5. Sydney 6. Melbourne
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
